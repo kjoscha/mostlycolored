ef56dd05559743d986362d191f552164c71fe36f
kjoscha <kjoscha@gmail.com>
2017-11-11 13:24:44 +0100

    allow hidden galleries

--- server.rb
+++ server.rb
@@ -7,12 +7,18 @@ use Rack::Auth::Basic, "Restricted Area" do |username, password|
 end
 
 get '/' do
-  @galleries = gallery_dir
+  @galleries = galleries
   haml :index
 end
 
+get '/:search_string' do
+  @galleries = galleries(params[:search_string])
+  haml :index
+end
+
+
 get '/galleries' do
-  halt 200, gallery_dir.to_json
+  halt 200, galleries.to_json
 end
 
 post '/save_image' do
@@ -20,23 +26,9 @@ post '/save_image' do
   file = params[:file][:tempfile]
   folder = params[:folder]
 
-  accepted_formats = ['.jpg', '.jpeg', '.JPG', '.png', '.PNG', '.gif']
-  unless file.size < 8_000_000 && accepted_formats.include?(File.extname(file))
-    return status 415
-  end
-
-  unless Dir.exist?("./public/images/#{folder}")
-    Dir.mkdir("./public/images/#{folder}")
-  end
-
-  path = "./public/images/#{folder}/#{filename}"
-
-  File.open(path, 'wb') do |f|
-    f.write(file.read)
-  end
-
-  image = MiniMagick::Image.new(path)
-  image.resize '1600x1600'
+  validate_file(file)
+  make_dir_if_not_exists(folder)
+  save_and_resize(folder, filename)
 
   status 200
 end
@@ -44,17 +36,45 @@ end
 post '/create_gallery' do
   name = params[:name]
   Dir.mkdir("./public/images/#{name}")
-  halt 200, gallery_dir.to_json
+  halt 200, galleries.to_json
 end
 
 private
 
-def gallery_dir
+def make_dir_if_not_exists(folder)
+  unless Dir.exist?("./public/images/#{folder}")
+    Dir.mkdir("./public/images/#{folder}")
+  end
+end
+
+def validate_file(file)
+  accepted_formats = ['.jpg', '.jpeg', '.JPG', '.png', '.PNG', '.gif']
+  unless file.size < 8_000_000 && accepted_formats.include?(File.extname(file))
+    return status 415
+  end
+end
+
+def save_and_resize(folder, filename)
+  path = "./public/images/#{folder}/#{filename}"
+  
+  File.open(path, 'wb') do |f|
+    f.write(file.read)
+  end
+
+  image = MiniMagick::Image.new(path)
+  image.resize '1600x1600'
+end
+
+def galleries(search_string = '')
   Dir['public/images/*'].map do |dir|
+    # galleries are hidden when '___' appears in name
+    # Right part of the name is the search string to unlock
+    next if search_string != dir.split('___')[1] && dir.include?('___')
+
     folder = dir.gsub('public/images/', '')
     images = Dir.glob("#{dir}/*").map { |image| image.gsub('public/', '') }
     last_changed_at = File.ctime(dir).to_s
 
     [folder, images, last_changed_at]
-  end.sort_by {|obj| obj[2]}
+  end.compact.sort_by {|obj| obj[2]}
 end
